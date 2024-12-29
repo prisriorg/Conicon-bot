b5cf9d51c83153f2df2cc68148a5917685292afc
Priyansh Srivastava <prisriorg@gmail.com>
2024-12-29 18:22:54 +0530

    first commit

--- src/components/tgapp/tabs/EarnTab.tsx
+++ src/components/tgapp/tabs/EarnTab.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Image from "next/image";
-import coin from "@/app/_assets/coin-3.svg"; 
+import coin from "@/app/_assets/coin-3.svg";
 
 const EarnTab = () => {
   const [activeTab, setActiveTab] = React.useState("ads");
@@ -30,10 +30,10 @@ const EarnTab = () => {
     <div className={`transition-all duration-300`}>
       <div className="flex gap-0 mt-4">
         <button
-          onClick={() => setActiveTab("in-game")}
+          onClick={() => setActiveTab("ads")}
           className={`flex-1 py-2 px-4 rounded-lg text-sm font-medium transition duration-300 
                         ${
-                          activeTab === "in-game"
+                          activeTab === "ads"
                             ? "bg-white text-black"
                             : "bg-[#151515] text-white"
                         }`}
@@ -58,45 +58,47 @@ const EarnTab = () => {
 
       {/* Tasks List */}
       <div className="mt-4 mb-20">
-        {(activeTab ==="partners"?partnerTasks:tasks).map((task, index) => (
-          <div
-            key={index}
-            className="flex items-center bg-[#151516]  mb-2  rounded-xl"
-          >
-            <div className="w-[72px] flex justify-center">
-              {" "}
-              {/* Fixed width container for icons */}
-              <div className="w-10 h-10  bg-black p-2 rounded-xl border">
-                {" "}
-                {/* Fixed size container */}
-                {typeof task.icon === "string" ? (
-                  <img
-                    src={task.icon}
-                    alt={task.title}
-                    width={40}
-                    height={40}
-                    className="w-full h-full object-contain"
-                  />
-                ) : (
-                  <span className="w-full h-full" />
-                )}
-              </div>
-            </div>
+        {(activeTab === "partners" ? partnerTasks : tasks).map(
+          (task, index) => (
             <div
-              className={`flex items-center justify-between w-full py-4 pr-4 ${
-                index !== 0 && "border-t border-[#222622]"
-              }`}
+              key={index}
+              className="flex items-center bg-[#151516]  mb-2  rounded-xl"
             >
-              <div>
-                <div className="text-[17px]">{task.title}</div>
-                <div className="text-gray-400 text-[14px]">{task.reward}</div>
+              <div className="w-[72px] flex justify-center">
+                {" "}
+                {/* Fixed width container for icons */}
+                <div className="w-10 h-10  bg-black p-2 rounded-xl border">
+                  {" "}
+                  {/* Fixed size container */}
+                  {typeof task.icon === "string" ? (
+                    <Image
+                      src={task.icon}
+                      alt={task.title}
+                      width={40}
+                      height={40}
+                      className="w-full h-full object-contain"
+                    />
+                  ) : (
+                    <span className="w-full h-full" />
+                  )}
+                </div>
               </div>
-              <button className="h-8 bg-white text-black px-4 rounded-full text-sm font-medium flex items-center">
-                Start
-              </button>
+              <div
+                className={`flex items-center justify-between w-full py-4 pr-4 ${
+                  index !== 0 && "border-t border-[#222622]"
+                }`}
+              >
+                <div>
+                  <div className="text-[17px]">{task.title}</div>
+                  <div className="text-gray-400 text-[14px]">{task.reward}</div>
+                </div>
+                <button className="h-8 bg-white text-black px-4 rounded-full text-sm font-medium flex items-center">
+                  Start
+                </button>
+              </div>
             </div>
-          </div>
-        ))}
+          )
+        )}
       </div>
     </div>
   );
