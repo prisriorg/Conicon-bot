0cb36a9ccb4163eb3e95756b611ee55730e3008f
Priyansh Srivastava <prisriorg@gmail.com>
2024-12-31 01:41:40 +0530

    first commit

--- src/app/api/init/route.ts
+++ src/app/api/init/route.ts
@@ -1,19 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
-import imageThumbnail from "image-thumbnail";
+// import imageThumbnail from "image-thumbnail";
 export const runtime = "edge";
 export async function POST(req: NextRequest) {
   let options = { percentage: 25, responseType: "base64" };
 
-  try {
-    const thumbnail = await imageThumbnail("https://cataas.com/cat", {
-      width: 50,
-      height: 50,
-      responseType: "base64",
-    });
-    console.log(thumbnail);
-  } catch (err) {
-    console.error(err);
-  }
+  // try {
+  //   const thumbnail = await imageThumbnail("https://cataas.com/cat", {
+  //     width: 50,
+  //     height: 50,
+  //     responseType: "base64",
+  //   });
+  //   console.log(thumbnail);
+  // } catch (err) {
+  //   console.error(err);
+  // }
   const ddd = await req.json();
   // console.log(ddd);
   return NextResponse.json({ ddd });
