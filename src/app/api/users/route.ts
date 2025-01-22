b1ee82332e246c37bea14483141b50f4dd97300b
Priyansh Srivastava <prisriorg@gmail.com>
2025-01-23 01:16:50 +0530

    Refactor GET handler to retrieve names from the database and return as JSON response

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,9 +1,11 @@
+import { db } from "@/db";
+import { fooTable } from "@/db/schema";
 import { NextResponse, type NextRequest } from "next/server";
 
 export const runtime = "edge";
 
-export async function POST(request: NextRequest) {
-  const { name } = await request.json();
+export async function GET(request: NextRequest) {
+  const name = await db.select().from(fooTable).all();
 
   return NextResponse.json(name);
 }
